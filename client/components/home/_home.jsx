bb52138a020ba94bcc14ca2be9353a6e8ad98ee1
Anne Jetton <annemariejetton@gmail.com>
2022-02-21 23:56:29 -0700

    .....happy colors?

--- client/components/home/_home.jsx
+++ client/components/home/_home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { ApiContext } from '../../utils/api_context';
@@ -62,52 +61,3 @@ export const Home = () => {
     </div>
   );
 };
-=======
-import { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-import { ApiContext } from '../../utils/api_context';
-import { AuthContext } from '../../utils/auth_context';
-import { RolesContext } from '../../utils/roles_context';
-import { Button } from '../common/button';
-
-export const Home = () => {
-  const [, setAuthToken] = useContext(AuthContext);
-  const api = useContext(ApiContext);
-  const roles = useContext(RolesContext);
-
-  const navigate = useNavigate();
-
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
-  useEffect(async () => {
-    const res = await api.get('/users/me');
-    setUser(res.user);
-    setLoading(false);
-  }, []);
-
-  const logout = async () => {
-    const res = await api.del('/sessions');
-    if (res.success) {
-      setAuthToken(null);
-    }
-  };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="p-4">
-      <h1>Welcome {user.firstName}</h1>
-      <Button type="button" onClick={logout}>
-        Logout
-      </Button>
-      {roles.includes('admin') && (
-        <Button type="button" onClick={() => navigate('/admin')}>
-          Admin
-        </Button>
-      )}
-    </div>
-  );
-};
->>>>>>> Stashed changes
