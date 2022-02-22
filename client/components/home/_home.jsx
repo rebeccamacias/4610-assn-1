7db6881d016eca45dea675bdac077e49e8a64274
Vinny <vincentmichaelsmith@gmail.com>
2022-02-22 15:42:37 -0700

    starting on newProject create function

--- client/components/home/_home.jsx
+++ client/components/home/_home.jsx
@@ -14,6 +14,8 @@ export const Home = () => {
 
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
+  const [newProjectName, setNewProjectName] = useState("");
+
   useEffect(async () => {
     const res = await api.get('/users/me');
     setUser(res.user);
@@ -35,8 +37,9 @@ export const Home = () => {
   var projectsDiv = projects.map((project)=> 
   <div key={project.project_id}>
       Name {/*Get Project name */}
+      Go to Project
       <Button type="button" onClick={(api.get('/project:id'))} /*Navigate to project view */>
-        Go to project name {project.name}
+        Go to project name {}
       </Button>
   </div>)
   
@@ -45,11 +48,18 @@ export const Home = () => {
       <h1>Welcome {user.firstName}</h1>
     <div>
       <h1>Projects</h1>
-      <Button type="button" onClick={api.null}>Create new project</Button>
+      
       {/*TODO Create project component, then get all projects the user is associated with */}
       {projectsDiv}
       
     </div>
+    <textarea
+        className="p-2 border-2 rounded flex"
+        value={newProjectName}
+        onChange={(e) => setNewProjectName(e.target.value)}
+      />
+    <Button type="button" onClick={createNewProject()}>Create new project</Button> 
+    
 
       <Button type="button" onClick={logout}>
         Logout
