66dbd97ec2c7cd0109451b2373dd2dc4708eb30a
Vinny <vincentmichaelsmith@gmail.com>
2022-02-24 15:28:40 -0700

    Stub function to toggle task completion, and removal of unnessacary comments

--- client/components/project/task.jsx
+++ client/components/project/task.jsx
@@ -1,18 +1,24 @@
 import { Button } from '../common/button';
+import { ApiContext } from '../../utils/api_context';
 
 export const Task = ({ task, ...other }) => {
+  const api = useContext(ApiContext);
+  const toggleCompletion = async () => {
+
+  }
+
   var button;
   if (task.status == true){
-    button = <Button className="border-2 rounded p-4 bg-red-600">Mark as undone</Button>
+    button = <Button className="border-2 rounded p-4 bg-red-600" onClick={toggleCompletion}>Mark as undone</Button>
   } else {
-    button = <Button className="border-2 rounded p-4 bg-green-600">Mark as done</Button>
+    button = <Button className="border-2 rounded p-4 bg-green-600" onClick={toggleCompletion}>Mark as done</Button>
   } 
   return (
       <div className="border-2 rounded p-4" {...other}>
-        {task.title}
-        {task.description}
-        {task.time_estimation}
-        Completed: {task.status}
+        {task.title}<br/>
+        {task.description}<br/>
+        {task.time_estimation}<br/>
+        Completed: {task.status}<br/>
         { button }
       </div>
     );
