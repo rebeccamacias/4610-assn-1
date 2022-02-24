f21dc9910aec13f186d7ca4262995fd6c2a6ac32
Vinny <vincentmichaelsmith@gmail.com>
2022-02-24 15:36:21 -0700

    Hooking up task looks to be a pain

--- client/components/project/task.jsx
+++ client/components/project/task.jsx
@@ -1,9 +1,20 @@
 import { Button } from '../common/button';
 import { ApiContext } from '../../utils/api_context';
+import { useContext, useEffect, useState } from 'react';
 
 export const Task = ({ task, users, ...other }) => {
   const api = useContext(ApiContext);
+  const [user, setUser] = useState(null);
+
+  useEffect(async () => {
+
+  });
+
   const toggleCompletion = async () => {
+
+  }
+
+  const assignTaskToUser = async () => {
 
   }
 
@@ -17,6 +28,7 @@ export const Task = ({ task, users, ...other }) => {
       <div className="border-2 rounded p-4" {...other}>
         {task.title}<br/>
         {task.description}<br/>
+        Assigned to {user.firstName}
         {task.time_estimation}<br/>
         Completed: {task.status}<br/>
         { button }
